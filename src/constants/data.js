460c1285f621ebfa0ad738e4e0c876fdbc6ba548
Amr <frank1276smith@gmail.com>
2024-02-06 21:46:19 +0200

    creatiing data.js file, and it contains the navbar data

---
+++ src/constants/data.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+    {label: 'Home', to: '/' },
+    {label: 'Transfer', to: '/transfer'},
+    {label: 'Income', to: '/income' },
+    {label: 'Contact us', to: '/contact'},
+]
