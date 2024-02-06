cc24ea0c30e0fca33ac379779ad7a6c5116fafcc
Amr <frank1276smith@gmail.com>
2024-02-06 23:43:16 +0200

    creating a custom hero section component to with image, overlay, and a div container for h1 and p tags.

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+function Hero({ src, title, description }) {
+  return (
+    <section className="relative">
+      <img
+        src={src}
+        className="object-cover object-center w-full h-[895px] md:h-[700px]"
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+      <div className="absolute inset-2 flex flex-col justify-center items-start text-white md:ml-8 md:w-1/2">
+        <h1 className="text-[68px] font-extrabold">{title}</h1>
+        <p className="font-medium text-[24px]">{description}</p>
+      </div>
+    </section>
+  );
+}
+export default Hero;
