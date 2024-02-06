2ec635eafba23535c63ed5530b41693a54bbc21a
Amr <frank1276smith@gmail.com>
2024-02-06 21:40:43 +0200

    adding our routing for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./Root.jsx";
+import Error from "./pages/Error.jsx";
+import Home from "./pages/Home.jsx";
+import Transfer from "./pages/Transfer.jsx";
+import Income from "./pages/Income.jsx";
+import Contact from "./pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/transfer", element: <Transfer /> },
+      { path: "/income", element: <Income /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+]);
+
 function App() {
-  return <h1>Hello</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
