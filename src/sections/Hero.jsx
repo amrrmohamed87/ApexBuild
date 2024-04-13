57293558ec68f22e73c14e154a2270c35818d98d
Amr <frank1276smith@gmail.com>
2024-04-13 16:47:16 +0200

    changing the hero image and customizing the hero section from the hero component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,32 +7,35 @@ function Hero({ src, title, description, id }) {
         src={src}
         className="object-cover object-center w-full h-[895px] md:h-[700px]"
       />
-      <div
+      {/* <div
         style={{
           position: "absolute",
           inset: 0,
           background:
             "linear-gradient(to right, rgba(99, 102, 241, 0.5), rgba(168, 85, 247, 0.5), rgba(236, 72, 153, 0.5))",
         }}
-      ></div>
-      <div className="absolute inset-0 bg-black bg-opacity-70"></div>
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-white md:ml-[24rem] md:w-1/2">
+      ></div> */}
+      {/*       <div className="absolute inset-0 bg-black bg-opacity-20"></div>
+       */}{" "}
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-white md:ml-[24rem] md:w-1/2 mt-40">
         <h1 className="text-[35px] md:text-[68px] tracking-widest font-extrabold">
           {title}
         </h1>
         <p className="font-medium text-[14px] ml-4 md:text-[24px] tracking-widest">
           {description}
         </p>
-        <div className="absolute mt-40  md:mt-60 flex justify-center items-center">
-          <button
-            style={{
-              background:
-                "linear-gradient(to right, rgba(99, 102, 241, 1), rgba(168, 85, 247, 0.7), rgba(236, 72, 153, 0.8))",
-            }}
-            className="px-12 py-2 rounded-lg"
-          >
-            <NavLink to="/login">Sign in</NavLink>
-          </button>
+        <div className="absolute mt-28 flex justify-center items-center">
+          <NavLink to="/login">
+            <button
+              style={{
+                background:
+                  "linear-gradient(to right, rgba(99, 102, 241, 1), rgba(168, 85, 247, 0.7), rgba(236, 72, 153, 0.8))",
+              }}
+              className="px-12 py-2 rounded-lg"
+            >
+              Sign in
+            </button>
+          </NavLink>
         </div>
       </div>
     </section>
